8fada8f6650bd58d76de900e04d2b77a7a5cbf16
Sharon Mwenda <smkathambi@gmail.com>
2022-06-21 22:13:02 +0300

    declaring variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // Code here
 //Introducing the DOMContentLoaded event
 document.addEventListener("DOMContentLoaded",() => {
-
+  const beerTitle = document.getElementById("beer-name")
+  const beerImage = document.getElementById("beer-image")
+  const beerDesc = document.getElementById("beer-description")
+  const reviewList = document.getElementById("review-list")
+        
 })
