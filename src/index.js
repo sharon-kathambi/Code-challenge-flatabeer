c8777a7ca7f5f1412570a264ba94be7291aaac87
Sharon Mwenda <smkathambi@gmail.com>
2022-06-21 22:15:42 +0300

    fetching data from server

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ document.addEventListener("DOMContentLoaded",() => {
   const beerImage = document.getElementById("beer-image")
   const beerDesc = document.getElementById("beer-description")
   const reviewList = document.getElementById("review-list")
-        
+    
+  // See the first beer's details, including its name, image, description, and reviews, when the page loads.
+    // fetch beer data from the server.  
+    fetch("http://localhost:3000/beers")
+    .then((res)=>res.json())
+    .then((beers)=>{
+      console.log(beers)
+    })
 })
