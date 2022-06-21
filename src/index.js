a50b78a1a185bed1b0be0e8762f0e9c611820316
Sharon Mwenda <smkathambi@gmail.com>
2022-06-21 22:20:36 +0300

    display beer one

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded",() => {
     fetch("http://localhost:3000/beers")
     .then((res)=>res.json())
     .then((beers)=>{
-      console.log(beers)
+      // display beer one
+      beerTitle.textContent = beers[0].name
+      //console.log(beers)
     })
 })
